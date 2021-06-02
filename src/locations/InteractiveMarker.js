829bb9a0e0ecdb76417663c683af8eba3ddbb72e
Alfred Roberts <robertsalfie14@gmail.com>
2021-06-02 12:32:16 +0100

    Fixed location map info popup

--- src/locations/InteractiveMarker.js
+++ src/locations/InteractiveMarker.js
@@ -7,21 +7,17 @@ import { BrowserRouter as Link } from "react-router-dom";
 class InteractiveMarker extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      showInfo: false,
-    }
-  }
-
-  markerClick = () => {
-    this.setState({showInfo: !this.state.showInfo})
+    // this.state = {
+    //   showInfo: false,
+    // }
   }
 
   render() {
-    let info_class = this.state.showInfo ? 'info_popup_show' : 'info_popup_hidden'
+    let info_class = (this.props.showInfo) ? 'info_popup_show' : 'info_popup_hidden'
     return (
       <div>
         <Marker latitude={this.props.photo.location.lat} longitude={this.props.photo.location.lng} offsetLeft={-15} offsetTop={-30}>
-          <img onClick={this.markerClick} src={process.env.PUBLIC_URL + "../marker.png"} width="30px" height="30px"/>
+          <img className='locationMarker' onClick={this.props.onClick} src={process.env.PUBLIC_URL + "../marker.png"} width="30px" height="30px"/>
             <div className={info_class} style={{backgroundColor: this.props.photo.accentColour}}>
               <a href={"/photo/"+ this.props.photo.title} className='locationLink'>{this.props.photo.title}</a>
             </div>
--- src/locations/MultiLocationViewer.js
+++ src/locations/MultiLocationViewer.js
@@ -16,15 +16,33 @@ class MultiLocationViewer extends React.Component {
         pitch: 0
       },
       markers: null,
+      showMarkerInfo: Array(this.props.photos.length).fill(false),
+      showMarkerInfoPrev: null,
     };
 
+  }
+
+  updateInfo = () => {
+    this.setState({showMarkerInfo: Array(this.props.photos.length).fill(false)})
+    console.log('map click')
+  }
+
+  markerClick = (i) => {
+    console.log('marker click')
+    const showMarkerInfo = this.state.showMarkerInfo.slice()
+    showMarkerInfo[i] = !showMarkerInfo[i];
+    if (this.state.showMarkerInfoPrev && this.state.showMarkerInfoPrev != i) {
+      showMarkerInfo[this.state.showMarkerInfoPrev] = false;
+    }
+    this.setState({showMarkerInfo: showMarkerInfo})
+    this.setState({showMarkerInfoPrev: i})
   }
   
   render() {
     let markers = []
-    for (let photo of this.props.photos) {
+    for (let i = 0; i < this.props.photos.length; i++) {
       markers.push(
-        <InteractiveMarker photo={photo} />
+        <InteractiveMarker photo={this.props.photos[i]} showInfo={this.state.showMarkerInfo[i]} onClick={() => this.markerClick(i)}/>
       );
     }
     return (
@@ -35,6 +53,7 @@ class MultiLocationViewer extends React.Component {
         mapStyle="mapbox://styles/alfredroberts/ckp9pct361c8p17pkbg6j74de"
         onViewportChange={viewport => this.setState({viewport})}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+        onClick={this.updateInfo}
       >
         {markers}
       </MapGL>
