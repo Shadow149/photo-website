406be4d93cb538593c4fd7d8cd6c583874a9e095
Alfred Roberts <robertsalfie14@gmail.com>
2024-01-03 14:40:43 +0000

    Add server listening

--- functions/server.js
+++ functions/server.js
@@ -11,20 +11,20 @@ app.use(express.json());
 app.use('/.netlify/functions/server', require("./routes/record"));
 // get driver connection
 const dbo = require("./db/conn");
-dbo.connectToServer(function (err) {
-  if (err) console.error(err);
-});
-console.log(`Server is running on port: ${port}`);
-
-
-
-// app.listen(port, () => {
-//   // perform a database connection when server starts
-//   dbo.connectToServer(function (err) {
-//     if (err) console.error(err);
-//   });
-//   console.log(`Server is running on port: ${port}`);
+// dbo.connectToServer(function (err) {
+//   if (err) console.error(err);
 // });
+// console.log(`Server is running on port: ${port}`);
+
+
+
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+  });
+  console.log(`Server is running on port: ${port}`);
+});
 
 // nodemon server
 
