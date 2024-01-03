82061a3e104d406419a6bd355d568116fa0495a6
Alfred Roberts <robertsalfie14@gmail.com>
2024-01-03 16:58:49 +0000

    Add vercel hosting

---
+++ server/db/conn.js
@@ -0,0 +1,30 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const Db = process.env.ATLAS_URI;
+const client = new MongoClient(encodeURI(Db), {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+    useUnifiedTopology: true,    
+  }
+});
+ 
+var _db;
+ 
+module.exports = {
+  connectToServer: function (callback) {
+    client.connect(function (err, db) {
+      // Verify we got a good "db" object
+      if (db)
+      {
+        _db = db.db("photos_db");
+        console.log("Successfully connected to MongoDB."); 
+      }
+      return callback(err);
+         });
+  },
+ 
+  getDb: function () {
+    return _db;
+  },
+};
--- server/index.js
+++ server/index.js
@@ -1,26 +1,22 @@
-'use strict';
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const serverless = require('serverless-http');
 
 require("dotenv").config({ path: "./.env" });
 
 const port = process.env.DB_PORT || 5000;
 app.use(cors());
 app.use(express.json());
-
+app.use(require("./routes/record"));
+// get driver connection
 const dbo = require("./db/conn");
-
-async function start() {
-  const result = await dbo.connectToServer();
+ 
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+  });
   console.log(`Server is running on port: ${port}`);
-  console.log(result)
-}
+});
 
-app.use('/.netlify/functions/server', require("./routes/record"));
-
-module.exports = app;
-module.exports.handler = serverless(app);
-
-start();
+// nodemon server
